2aacb263b6c8dd9d1cc134d0830190134dfee5e4
Kyle Andrews <kylea@page.works>
2019-07-12 11:13:16 -0400

    Adds drag start and end event listeners

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -1,2 +1,46 @@
-console.log('Hello world');
+var Carousel = /** @class */ (function () {
+    function Carousel() {
+        this.handleDragStart = this.startDragging.bind(this);
+        this.handleDragStop = this.stopDragging.bind(this);
+        this.handleDrag = this.dragging.bind(this);
+        this._carousel = document.body.querySelector('carousel');
+        this._slides = Array.from(document.body.querySelectorAll('slide'));
+        this._mouse = null;
+        this.init();
+    }
+    Carousel.prototype.init = function () {
+        for (var i = 0; i < this._slides.length; i++) {
+            this._slides[i].addEventListener('dragstart', this.handleDragStart);
+            this._slides[i].addEventListener('dragend', this.handleDragStop);
+            this._slides[i].addEventListener('drag', this.handleDrag);
+        }
+    };
+    Carousel.prototype.startDragging = function (e) {
+        e.preventDefault();
+        console.log('Started Dragging');
+        console.log(e);
+        this._carousel.classList.add('is-dragging');
+        this._mouse = {
+            x: e.x,
+            y: e.y
+        };
+    };
+    Carousel.prototype.stopDragging = function (e) {
+        e.preventDefault();
+        console.log('Stopped Dragging');
+        console.log(e);
+        this._carousel.classList.remove('is-dragging');
+    };
+    Carousel.prototype.dragging = function (e) {
+        e.preventDefault();
+        console.log(e);
+        var newMouse = {
+            x: e.x,
+            y: e.y
+        };
+        console.log((newMouse.x - this._mouse.x), (newMouse.y - this._mouse.y));
+    };
+    return Carousel;
+}());
+new Carousel();
 //# sourceMappingURL=main.js.map
--- docs/main.ts
+++ docs/main.ts
@@ -1,1 +1,67 @@
-console.log('Hello world');
+interface Mouse{
+    x: number;
+    y: number;
+}
+
+class Carousel
+{
+    private _carousel : HTMLElement;
+    private _slides : Array<HTMLElement>;
+    private _mouse : Mouse;    
+
+    constructor()
+    {
+        this._carousel = document.body.querySelector('carousel');
+        this._slides = Array.from(document.body.querySelectorAll('slide'));
+        this._mouse = null;
+        this.init();
+    }
+
+    private handleDragStart:EventListener = this.startDragging.bind(this);
+    private handleDragStop:EventListener = this.stopDragging.bind(this);
+    private handleDrag:EventListener = this.dragging.bind(this);
+
+    private init() : void
+    {
+        for(let i = 0; i < this._slides.length; i++)
+        {
+            this._slides[i].addEventListener('dragstart', this.handleDragStart);
+            this._slides[i].addEventListener('dragend', this.handleDragStop);
+            this._slides[i].addEventListener('drag', this.handleDrag);
+        }
+    }
+
+    private startDragging(e:DragEvent) : void
+    {
+        e.preventDefault();
+        console.log('Started Dragging');
+        console.log(e);
+        this._carousel.classList.add('is-dragging');
+        this._mouse = {
+            x: e.x,
+            y: e.y
+        }
+    }
+
+    private stopDragging(e:DragEvent) : void
+    {
+        e.preventDefault();
+        console.log('Stopped Dragging');
+        console.log(e);
+        this._carousel.classList.remove('is-dragging');
+    }
+
+    private dragging(e:DragEvent) : void
+    {
+        e.preventDefault();
+        console.log(e);
+        const newMouse = {
+            x: e.x,
+            y: e.y
+        }
+
+        console.log((newMouse.x - this._mouse.x), (newMouse.y - this._mouse.y));
+    }
+}
+
+new Carousel();
