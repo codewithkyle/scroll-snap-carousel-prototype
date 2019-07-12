ad480f2977485d3442bf4fb03f713e8636d80548
Kyle Andrews <kylea@page.works>
2019-07-12 11:22:16 -0400

    Updates dragging detection on desktop

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -6,39 +6,36 @@ var Carousel = /** @class */ (function () {
         this._carousel = document.body.querySelector('carousel');
         this._slides = Array.from(document.body.querySelectorAll('slide'));
         this._mouse = null;
+        this._dragging = false;
         this.init();
     }
     Carousel.prototype.init = function () {
         for (var i = 0; i < this._slides.length; i++) {
             this._slides[i].addEventListener('dragstart', this.handleDragStart);
-            this._slides[i].addEventListener('dragend', this.handleDragStop);
-            this._slides[i].addEventListener('drag', this.handleDrag);
+            this._slides[i].addEventListener('mouseup', this.handleDragStop);
         }
+        this._carousel.addEventListener('mousemove', this.handleDrag, { passive: true });
     };
     Carousel.prototype.startDragging = function (e) {
         e.preventDefault();
         console.log('Started Dragging');
-        console.log(e);
         this._carousel.classList.add('is-dragging');
-        this._mouse = {
-            x: e.x,
-            y: e.y
-        };
+        this._mouse = { x: e.x, y: e.y };
+        this._dragging = true;
     };
     Carousel.prototype.stopDragging = function (e) {
-        e.preventDefault();
-        console.log('Stopped Dragging');
-        console.log(e);
-        this._carousel.classList.remove('is-dragging');
+        if (this._dragging) {
+            console.log('Stopped Dragging');
+            this._carousel.classList.remove('is-dragging');
+            this._dragging = false;
+            this._mouse = null;
+        }
     };
     Carousel.prototype.dragging = function (e) {
-        e.preventDefault();
-        console.log(e);
-        var newMouse = {
-            x: e.x,
-            y: e.y
-        };
-        console.log((newMouse.x - this._mouse.x), (newMouse.y - this._mouse.y));
+        if (this._dragging) {
+            var newMouse = { x: e.x, y: e.y };
+            console.log((newMouse.x - this._mouse.x), (newMouse.y - this._mouse.y));
+        }
     };
     return Carousel;
 }());
--- docs/main.ts
+++ docs/main.ts
@@ -7,13 +7,15 @@ class Carousel
 {
     private _carousel : HTMLElement;
     private _slides : Array<HTMLElement>;
-    private _mouse : Mouse;    
+    private _mouse : Mouse;
+    private _dragging : boolean;
 
     constructor()
     {
         this._carousel = document.body.querySelector('carousel');
         this._slides = Array.from(document.body.querySelectorAll('slide'));
         this._mouse = null;
+        this._dragging = false;
         this.init();
     }
 
@@ -26,41 +28,39 @@ class Carousel
         for(let i = 0; i < this._slides.length; i++)
         {
             this._slides[i].addEventListener('dragstart', this.handleDragStart);
-            this._slides[i].addEventListener('dragend', this.handleDragStop);
-            this._slides[i].addEventListener('drag', this.handleDrag);
+            this._slides[i].addEventListener('mouseup', this.handleDragStop);
         }
+
+        this._carousel.addEventListener('mousemove', this.handleDrag, { passive: true });
     }
 
     private startDragging(e:DragEvent) : void
     {
         e.preventDefault();
         console.log('Started Dragging');
-        console.log(e);
         this._carousel.classList.add('is-dragging');
-        this._mouse = {
-            x: e.x,
-            y: e.y
+        this._mouse = { x: e.x, y: e.y };
+        this._dragging = true;
+    }
+
+    private stopDragging(e:MouseEvent) : void
+    {
+        if(this._dragging)
+        {
+            console.log('Stopped Dragging');
+            this._carousel.classList.remove('is-dragging');
+            this._dragging = false;
+            this._mouse = null;
         }
     }
 
-    private stopDragging(e:DragEvent) : void
+    private dragging(e:MouseEvent) : void
     {
-        e.preventDefault();
-        console.log('Stopped Dragging');
-        console.log(e);
-        this._carousel.classList.remove('is-dragging');
-    }
-
-    private dragging(e:DragEvent) : void
-    {
-        e.preventDefault();
-        console.log(e);
-        const newMouse = {
-            x: e.x,
-            y: e.y
+        if(this._dragging)
+        {
+            const newMouse = { x: e.x, y: e.y };
+            console.log((newMouse.x - this._mouse.x), (newMouse.y - this._mouse.y));
         }
-
-        console.log((newMouse.x - this._mouse.x), (newMouse.y - this._mouse.y));
     }
 }
 
